41e351bac2cab4a49e4ebbc86421f6357dd8b552
ishiik1627 <149481261+ishiik1627@users.noreply.github.com>
2024-06-25 15:10:05 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // カウントダウンの終了日時を設定
+    var countDownDate = new Date("Dec 31, 2024 23:59:59").getTime();
+
+    // 毎秒カウントダウンを更新
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+});
